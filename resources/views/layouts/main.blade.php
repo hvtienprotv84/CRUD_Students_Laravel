98a5f5037cd4736a0b8691bfc6a897881246f4f7
hvtienprotv84 <dmx031117@gmail.om>
2024-07-22 05:58:49 +0700

    push all

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,7 +27,7 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link active" id="href_students" href="{{ route('students.index') }}">Students</a>
+                    <a class="nav-link active" id="href_students" href="{{ route('students.index') }}">👩🏻‍💻Students</a>
                 </li>
             </ul>
         </div>
